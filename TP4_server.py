11d52c48d9501f0b03fdcb35a63bfb3d0ee52abe
Louis-Etienne <louis-etienne.messier.1@ulaval.ca>
2024-11-18 10:09:56 -0500

    tesing

--- TP4_server.py
+++ TP4_server.py
@@ -311,8 +311,7 @@ class Server:
         else:
             nom_destinataire = payload["destination"][:-len(gloutils.SERVER_DOMAIN)-1].lower() # remove the SERVER_DOMAIN ending
             print("Envoi de courriel vers : ", nom_destinataire)
-            folderpaths = glob.glob(gloutils.SERVER_DATA_DIR)
-            folderpaths_name = [os.path.basename(x).lower() for x in folderpaths]
+            folderpaths_name = self._get_list_users()
             
             if not nom_destinataire in folderpaths_name:
                 output_message = gloutils.GloMessage(
